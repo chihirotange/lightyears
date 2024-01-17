be53a8087ad832804e57af82b51d751274f4a845
ai-chan <ohanasakuiroha@gmail.com>
2024-01-18 01:32:35 +0700

    added basic data structures

--- AiEngine/include/framework/Core.h
+++ AiEngine/include/framework/Core.h
@@ -1,5 +1,21 @@
 #pragma once
 #include <stdio.h>
+#include <memory>
+#include <unordered_map>
+template<typename T>
+using unique = std::unique_ptr<T>;
+
+template<typename T>
+using shared = std::shared_ptr<T>;
+
+template<typename T>
+using weak = std::weak_ptr<T>;
+
+template<typename T>
+using List = std::vector<T>;
+
+template<typename key, typename value, typename hash = std::hash<key>>
+using Map = std::map<key, value, hash>;
 
 namespace AiEngine
 {
